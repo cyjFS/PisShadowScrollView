b1ed0da2c606c966e15ba7c2c85c2009618be019
newegg <newegg@neweggdeMac-mini.local>
2015-07-17 09:17:08 +0800

    Initial Commit

---
+++ PisShadowScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PisShadowScrollView
+//
+//  Created by newegg on 15/7/17.
+//  Copyright (c) 2015年 newegg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PisShadowScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PisShadowScrollView
+//
+//  Created by newegg on 15/7/17.
+//  Copyright (c) 2015年 newegg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
